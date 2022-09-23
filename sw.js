d6a65c0e75aeea7efb01f4668ce9479404fab927
playgooglecom423 <114161533+playgooglecom423@users.noreply.github.com>
2022-09-23 13:48:32 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -28,32 +28,3 @@ self.addEventListener('fetch', evt => {
     })
   );
 });
-
-// событие install01
-self.addEventListener('install01', evt => {
-  evt.waitUntil(
-    caches.open(staticCacheName).then((cache) => {
-      console.log('caching shell assets');
-      cache.addAll(assets);
-    })
-  );
-});
-// событие activate
-self.addEventListener('activate', evt => {
-  evt.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(keys
-        .filter(key => key !== staticCacheName)
-        .map(key => caches.delete(key))
-      );
-    })
-  );
-});
-// событие fetch
-self.addEventListener('fetch', evt => {
-  evt.respondWith(
-    caches.match(evt.request).then(cacheRes => {
-      return cacheRes || fetch(evt.request);
-    })
-  );
-});
