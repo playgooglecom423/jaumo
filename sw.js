ecadad673fdaced4379650e476e8c901dc8ccf84
playgooglecom423 <114161533+playgooglecom423@users.noreply.github.com>
2022-09-23 14:39:00 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -9,15 +9,6 @@ self.addEventListener('install', evt => {
     })
   );
 });
-// событие load
-self.addEventListener('load', evt => {
-  evt.waitUntil(
-    caches.open(staticCacheName).then((cache) => {
-      console.log('caching shell assets');
-      cache.addAll(assets);
-    })
-  );
-});
 // событие activate
 self.addEventListener('activate', evt => {
   evt.waitUntil(
